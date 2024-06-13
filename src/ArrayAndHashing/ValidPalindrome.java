28acbf812ff8fd4b4e0b215a6c234b2a7fbfa481
Bevan Lewis <blew0009@student.monash.edu>
2024-06-13 14:20:08 +1000

    updated comments for validPalindrome

--- src/ArrayAndHashing/ValidPalindrome.java
+++ src/ArrayAndHashing/ValidPalindrome.java
@@ -2,12 +2,12 @@ package ArrayAndHashing;
 
 public class ValidPalindrome {
     public boolean isPalindrome(String s) {
-        // loop through the given string and take out all the non alpha num values
-        // check if this string and it's reverse are equal
-
+        // Create two pointers
         int leftPointer = 0;
         int rightPointer = s.length() - 1;
 
+        // pointers move from either side of the string and check if the
+        // characters are either letter or digit and if they are equal in lower case
         while (leftPointer < rightPointer) {
             char leftChar = s.charAt(leftPointer);
             char rightChar = s.charAt(rightPointer);
